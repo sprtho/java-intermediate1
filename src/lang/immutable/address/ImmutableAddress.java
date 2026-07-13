68a1aabc22bf74b06ea48d18e980d617d0facb8a
sprtho <sprtho@naver.com>
2026-07-13 16:34:39 +0900

    2026-07-13 Immutable

---
+++ src/lang/immutable/address/ImmutableAddress.java
@@ -0,0 +1,21 @@
+package lang.immutable.address;
+
+public class ImmutableAddress {
+
+    private final String value; //불변클래스
+
+    public ImmutableAddress(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "value='" + value + '\'' +
+                '}';
+    }
+}
